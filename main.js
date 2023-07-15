ea2b6f68b53d81c062fda6ddbd1cad3a95c63b9a
lauta02 <lauchaafernandez899@gmail.com>
2023-07-15 02:08:11 -0300

    Se agrega el Proyecto

---
+++ main.js
@@ -0,0 +1,9 @@
+function resaltar(elemento) {
+    elemento.querySelector('.caja3').classList.add('resaltado');
+  }
+  
+  function desresaltar(elemento) {
+    elemento.querySelector('.caja3').classList.remove('resaltado');
+  }
+
+  
